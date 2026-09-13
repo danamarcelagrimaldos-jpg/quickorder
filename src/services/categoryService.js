d84eb0e6743b71b4354511f4bba77c3f5878555e
Dana Grimaldos <danamarcelagrimaldos@gmail.com>
2026-09-13 11:21:21 -0500

    Actualizando url del mock api

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://6a9439890e895b145e5f552f.mockapi.io/categoria';
+const API_URL = 'https://6a9845047160beda2292d62e.mockapi.io/categorias';
 
 export const obtenerCategorias = () => {
   return fetch(API_URL)
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://6a9439890e895b145e5f552f.mockapi.io/producto';
+const API_URL = 'https://6a9845047160beda2292d62e.mockapi.io/productos';
 
 // Obtener todos los productos
 export const obtenerProductos = () => {
